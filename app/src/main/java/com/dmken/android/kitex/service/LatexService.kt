5de0e8a4dee8ed8ca44becc4d2d4910a46a7305d
Fabian Damken <fabian.damken@gmx.de>
2018-04-04 01:54:19 +0200

    Switched to own service.

--- app/src/main/java/com/dmken/android/kitex/service/LatexService.kt
+++ app/src/main/java/com/dmken/android/kitex/service/LatexService.kt
@@ -1,27 +1,26 @@
 package com.dmken.android.kitex.service
 
+import android.util.Log
 import okhttp3.*
 import java.io.IOException
 import java.io.InputStream
 import java.net.URLEncoder
+import java.util.logging.Logger
 
 class LatexService {
-    // FIXME: Use own service and do better validation.
-
     companion object {
-        val BASE_URL = "http://www.texrendr.com/cgi-bin/mathtex.cgi?\\dpi{4096}"
+        private const val TAG = "LatexService"
 
-        val HEADER_CONTENT_TYPE = "Content-Type"
-        val HEADER_CONTENT_TYPE_VALID = "image/gif";
-
-        // TODO: Extract encoding here.
-        fun createEquationUrl(code: String): String = BASE_URL + URLEncoder.encode(code, "UTF-8")
+        private const val SECRET_TOKEN = "dsoifhgsfzugsafi76t32oiuro872z308r0932urigsa8fdii8t3o872gfi7ewgafai8sg8632t8fzgsai8fh8fg32"
+        val BASE_URL = "https://www.dmken.com/kitex/?token=$SECRET_TOKEN"
     }
 
     private val client = OkHttpClient.Builder().build()
 
     fun retrieveEquation(code: String, callback: (LatexState, InputStream?) -> Unit) {
         // Thread: ?
+
+        Log.d(TAG, "Rendering <$code>.")
 
         client.newCall(makeRequest(code)).enqueue(object : Callback {
             // Thread: Web
@@ -32,10 +31,31 @@ class LatexService {
 
             override fun onResponse(call: Call, response: Response) {
                 when {
-                    !response.isSuccessful -> return callback(LatexState.SERVER_ERROR, null)
-                    response.header("Content-Length") == "145" -> return callback(LatexState.INVALID_EQUATION, null)
-                    response.header(HEADER_CONTENT_TYPE) == HEADER_CONTENT_TYPE_VALID -> handle(response, callback)
-                    else -> callback(LatexState.SERVER_ERROR, null)
+                    response.isSuccessful -> {
+                        Log.d(TAG, "Rendering of <$code> was successful.")
+
+                        return handle(response, callback)
+                    }
+                    response.code() == 400 -> {
+                        Log.d(TAG, "Rendering of <$code> failed (invalid equation).")
+
+                        return callback(LatexState.INVALID_EQUATION, null)
+                    }
+                    response.code() == 401 -> {
+                        Log.e(TAG, "Secret token was not accepted by the server (error 401).")
+
+                        return callback(LatexState.SERVER_ERROR, null)
+                    }
+                    response.code() == 405 -> {
+                        Log.e(TAG, "INTERNAL ERROR: Wrong HTTP method was used (error 405).")
+
+                        return callback(LatexState.SERVER_ERROR, null)
+                    }
+                    else -> {
+                        Log.e(TAG, "Internal server error (code ${response.code()}.")
+
+                        return callback(LatexState.SERVER_ERROR, null)
+                    }
                 }
             }
         })
@@ -46,7 +66,12 @@ class LatexService {
         return callback(LatexState.SUCCESSFUL, body.byteStream())
     }
 
-    private fun makeRequest(code: String): Request = Request.Builder().url(createEquationUrl(code)).build()
+    private fun makeRequest(code: String): Request {
+        return Request.Builder()
+                .url(BASE_URL)
+                .post(RequestBody.create(MediaType.parse("text/latex"), "\\begin{equation*} $code \\end{equation*}"))
+                .build()
+    }
 
     enum class LatexState {
         SUCCESSFUL,
